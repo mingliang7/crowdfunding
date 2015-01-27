229d16cd02eeaa5ff70b4dc1128db4da82c5d88c
kevin <faithful.7up@gmail.com>
2015-01-27 15:50:15 +0700

    refacotoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-before_action :set_cate
+before_action :set_category
   protect_from_forgery with: :exception
-  def set_cate
+  def set_category
   	@categories = Category.all
   end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+
   end
 
   # GET /categories/1
